22df411e057f5ddfbc082d43cf4eac0b3097d8af
Subhash Bhambu <103734118+Subhash5186@users.noreply.github.com>
2023-04-17 23:04:05 +0530

    Update os_p1.c

--- Desktop/os/os_p1.c
+++ Desktop/os/os_p1.c
@@ -15,43 +15,69 @@ int main()
     ingredients = (chef * (r - 1)) + 1;
 
     int ingredients_allocated[chef];
+    int sum1 = 0;
+    int ingredients_required[chef];
 
     for (int i = 0; i < chef; i++)
     {
-        printf("enter ingredients_allocated for chef %d ", i + 1);
-        scanf("%d", &ingredients_allocated[i]);
-        /* if(ingredients_allocated[i]>r){
-             printf("Chef can take atmost r ingredients ")
-             return 0;
-         }*/
+        printf(" the number of resources chef %d requires is   : ", i + 1);
+        scanf("%d", &ingredients_required[i]);
+        sum1 = sum1 + ingredients_required[i];
     }
+    if (sum1 == r * chef)
+    {
+        for (int i = 0; i < chef; i++)
+        {
+            ingredients_allocated[i] = r - 1;
+            printf("the resources allocated to  chef %d : %d \n", i + 1, ingredients_allocated[i]);
+        }
+    }
+
+    if (sum1 < (r * chef) && sum1 > (r - 1) * chef)
+    {
+        for (int i = 0; i < chef; i++)
+        {
+            if (i == 0)
+            {
+                ingredients_allocated[i] = ingredients_required[i];
+                printf("the resources allocated to  chef %d : %d \n", i + 1, ingredients_allocated[i]);
+            }
+            else
+            {
+                ingredients_allocated[i] = ingredients_required[i] - 1;
+                printf("the resources allocated to  chef %d : %d \n", i + 1, ingredients_allocated[i]);
+            }
+        }
+    }
+    if (sum1 <= (r - 1) * chef)
+    {
+        for (int i = 0; i < chef; i++)
+        {
+            ingredients_allocated[i] = ingredients_required[i];
+            printf("the resource allocated to each chef %d : %d \n", i + 1, ingredients_allocated[i]);
+        }
+    }
+
     for (int i = 0; i < chef; i++)
     {
         sum = sum + ingredients_allocated[i];
     }
 
-    if ((sum - chef + 1) > ingredients)
+    if ((sum + 1) > ingredients)
     {
-        printf("some chef are requesting more ingredients than the atmost value that is %d so there will be deadlock", r);
+        printf("some chef are requesting more ingredients than the atmost value that is %d ", r);
+        printf("There will be deadlock");
         return 0;
     }
-    int ingredients_available = 0;
-    int a = ingredients - sum;
-    if (a > 0)
-    {
-        int ingredients_available = ingredients - sum;
-    }
-    else
-    {
-        int ingredients_available = ingredients - (sum - chef);
-    }
+    int ingredients_available = ingredients - sum;
+    printf("ingredients available is %d\n", ingredients_available);
 
     for (int i = 0; i < chef; i++)
     {
-        printf("ingredients available is %d\n", ingredients_available);
+
         printf("chef %d done cooking\n", i + 1);
         ingredients_available = ingredients_allocated[i] + ingredients_available;
-        count++;
+        printf("ingredients available is %d\n", ingredients_available);
     }
 
     return 0;
